ed9f330b7b08724aa946734aee5c695c75a19a12
davidcloudobi <davidcloudobi@gmail.com>
2019-11-18 15:27:04 +0100

    setup login route for user interface

--- users.py
+++ users.py
@@ -10,7 +10,7 @@ from .helpers import apology
 def hello():
     return render_template("index.html")
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register.html", methods=["GET", "POST"])
 def register():
     """Register user"""
     if request.method=='GET':
@@ -37,3 +37,27 @@ def register():
         row = db.execute(f"Select email from users where email='{email}'")
         print(row)
         return redirect("/userdashboard")
+# @app.route('/login.html')
+# def login():
+#     return render_template('login.html')
+
+@app.route('/login.html', methods=['POST', 'GET'])
+
+def login_post():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = db.execute(f"SELECT * FROM users WHERE email = '{email}'")
+        
+        if len(user):
+            if check_password_hash(user[0]["pass_word"], password):
+                session['user_id'] = user[0]['id']
+                session['user_email'] = user[0]['email']
+                return redirect('/')
+            else:
+                return apology("invalid email or password")
+        else:
+            return apology("invalid email or password")
+    else:
+        return render_template('login.html')
