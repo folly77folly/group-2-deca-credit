12b3b4a1c5b1f02c7f3bdfe70befeb6f30106604
davidcloudobi <davidcloudobi@gmail.com>
2019-11-19 10:17:50 +0100

    setup pending loan route

--- admin.py
+++ admin.py
@@ -1,3 +1,12 @@
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session,json
 from flask_session import Session
+from . import app,db
+
+
+
+@app.route('/request')
+def pending():
+    status = "pending"
+    pending = db.execute(f"SELECT * FROM loan WHERE status = '{status}'")
+    return render_template("request.html", pending=pending)
